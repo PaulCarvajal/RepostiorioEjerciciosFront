476b9010fb0221c4f214be492cd3264c562b71d1
Paul <paulcarva2@hotmail.com>
2025-03-26 09:06:22 +0100

    chore(): exercise complete

---
+++ Ejercicios1_Alebat/ejercicios.js
@@ -0,0 +1,10 @@
+//Escribe una varible llamada user name utilizando las siguientes nomenclaturas:
+console.log("Camel case: userName")
+console.log("Pascal case: UserName")
+console.log("Snake case: user-name")
+console.log("Kebab case: user-name")
+
+//Define los siguientes tipos de variables, di cuál utilizarías y por qué.
+console.log("Var: variable de ambito global que puede ser muy peligrosa ya que puede ser reasignada en cualquier momento y causaria errores")
+console.log("Let: variable de ambito block tambien puede ser reasignada pero no permite tener dos variables del mismo nombre, se suelen utilizar en cosas como contadores o boolean, basicamente variables que pueda cambiar")
+console.log("Const: Es la que se suele utilizar ya que no permite ser reasignada parecido a un final de java")
